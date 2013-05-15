10da5e46a348498aa3e251b5f42172f387f30bed
ahanna <ach52@pitt.edu>
2013-05-15 11:15:24 -0300

    Update RarePlot.R
    I uploaded the wrong function editing to correct one.

--- src/RarePlot.R
+++ src/RarePlot.R
@@ -1,120 +1,50 @@
-RarePlot {graphics}                                                          R Documentation
-
-Plot Rarefraction Data
-Description
-
-Plot .groups.RareFaction Data from mothur''s rarefaction.single function..
-
-Usage
-
-RarePlot(x,y,ylab= "Number of Different OTUs",xlab= "Number of Tags Sampled",
-         pch= NA, xlim = NULL, ylim = NULL, error=FALSE) #Same Defaults as matplot
-
-Arguments
-
-x
-The .group.rarefaction file which is mothur''s rarefaction.single''s output 
-y
-The names of the the groups ie.  m6558d21, NOT including the X.0.03 and NOT the Lower Or Higher Bounds. 
-For multiple groups you want c("Example1","Example2",...) 
-
-type	
-character string (length 1 vector) or vector of 1-character strings indicating the type of plot for each column of y, see plot for all possible types. The first character of type defines the first plot, the second character the second, etc. Characters in type are cycled through; e.g., "pl" alternately plots points and lines.
-
-lty,lwd,lend	
-vector of line types, widths, and end styles. The first element is for the first column, the second element for the second column, etc., even if lines are not plotted for all columns. Line types will be used cyclically until all plots are drawn.
-
-pch	
-character string or vector of 1-characters or integers for plotting characters, see points. The first character is the plotting-character for the first plot, the second for the second, etc. The default is the digits (1 through 9, 0) then the lowercase and uppercase letters.
-
-col	
-vector of colors. Colors are used cyclically.
-
-cex	
-vector of character expansion sizes, used cyclically. This works as a multiple of par("cex"). NULL is equivalent to 1.0.
-
-bg	
-vector of background (fill) colors for the open plot symbols given by pch=21:25 as in points. The default NA corresponds to the one of the underlying function plot.xy.
-
-xlab, ylab	
-titles for x and y axes, as in plot.
-
-xlim, ylim	
-ranges of x and y axes, as in plot.
-
-...	
-Graphical parameters (see par) and any further arguments of plot, typically plot.default, may also be supplied as arguments to this function. Hence, the high-level graphics control arguments described under par and the arguments to title may be supplied to this function.
-
-add	
-logical. If TRUE, plots are added to current one, using points and lines.
-
-verbose	
-logical. If TRUE, write one line of what is done.
-
-Details
-
-Points involving missing values are not plotted.
-
-The first column of x is plotted against the first column of y, the second column of x against the second column of y, etc. If one matrix has fewer columns, plotting will cycle back through the columns again. (In particular, either x or y may be a vector, against which all columns of the other argument will be plotted.)
-
-The first element of col, cex, lty, lwd is used to plot the axes as well as the first line.
-
-Because plotting symbols are drawn with lines and because these functions may be changing the line style, you should probably specify lty=1 when using plotting symbols.
-
-Side Effects
-
-Function matplot generates a new plot; matpoints and matlines add to the current one.
-
-References
-
-Becker, R. A., Chambers, J. M. and Wilks, A. R. (1988) The New S Language. Wadsworth & Brooks/Cole.
-
-See Also
-
-plot, points, lines, matrix, par.
-
-Examples
-
-require(grDevices)
-matplot((-4:5)^2, main = "Quadratic") # almost identical to plot(*)
-sines <- outer(1:20, 1:4, function(x, y) sin(x / 20 * pi * y))
-matplot(sines, pch = 1:4, type = "o", col = rainbow(ncol(sines)))
-matplot(sines, type = "b", pch = 21:23, col = 2:5, bg = 2:5,
-        main = "matplot(...., pch = 21:23, bg = 2:5)")
-
-x <- 0:50/50
-matplot(x, outer(x, 1:8, function(x, k) sin(k*pi * x)),
-        ylim = c(-2,2), type = "plobcsSh",
-        main= "matplot(,type = \"plobcsSh\" )")
-## pch & type =  vector of 1-chars :
-matplot(x, outer(x, 1:4, function(x, k) sin(k*pi * x)),
-        pch = letters[1:4], type = c("b","p","o"))
-
-lends <- c("round","butt","square")
-matplot(matrix(1:12, 4), type="c", lty=1, lwd=10, lend=lends)
-text(cbind(2.5, 2*c(1,3,5)-.4), lends, col= 1:3, cex = 1.5)
-
-table(iris$Species) # is data.frame with 'Species' factor
-iS <- iris$Species == "setosa"
-iV <- iris$Species == "versicolor"
-op <- par(bg = "bisque")
-matplot(c(1, 8), c(0, 4.5), type= "n", xlab = "Length", ylab = "Width",
-        main = "Petal and Sepal Dimensions in Iris Blossoms")
-matpoints(iris[iS,c(1,3)], iris[iS,c(2,4)], pch = "sS", col = c(2,4))
-matpoints(iris[iV,c(1,3)], iris[iV,c(2,4)], pch = "vV", col = c(2,4))
-legend(1, 4, c("    Setosa Petals", "    Setosa Sepals",
-               "Versicolor Petals", "Versicolor Sepals"),
-       pch = "sSvV", col = rep(c(2,4), 2))
-
-nam.var <- colnames(iris)[-5]
-nam.spec <- as.character(iris[1+50*0:2, "Species"])
-iris.S <- array(NA, dim = c(50,4,3),
-                dimnames = list(NULL, nam.var, nam.spec))
-for(i in 1:3) iris.S[,,i] <- data.matrix(iris[1:50+50*(i-1), -5])
-
-matplot(iris.S[,"Petal.Length",], iris.S[,"Petal.Width",], pch="SCV",
-        col = rainbow(3, start = .8, end = .1),
-        sub = paste(c("S", "C", "V"), dimnames(iris.S)[[3]],
-                    sep = "=", collapse= ",  "),
-        main = "Fisher's Iris Data")
-par(op)
+RarePlot <- function(x,y,ylab= "Number of Different OTUs",xlab= "Number of Tags Sampled",pch= NA, xlim = NULL, ylim = NULL, error=FALSE) #Same Defaults as matplot
+{
+  library(reshape2)
+  require(ggplot2)#install.packages("ggplot2")
+  Data <- read.table(x, header=T)
+  Length <- length(Data[1,])
+  if (error == FALSE)
+  {
+    Steps <- seq(from=2,to=Length-2,by=3)
+    TrueDat <- Data[,c(1,Steps)]
+    Names <- names(TrueDat)
+    substr(names(TrueDat), 1, 6) <- ""
+    substr(names(TrueDat), 1, 6) <- ""
+    substr(names(TrueDat), 1, 6) <- ""
+    substr(names(TrueDat), 1, 6) <- ""
+    substr(names(TrueDat), 1, 6) <- ""
+    substr(names(TrueDat), 1, 6) <- ""
+    names(TrueDat)[1] <- "Number"
+    Number <- TrueDat[1]
+    Columns <- TrueDat[-1]
+    matplot(Number,Columns[,y],ylab= ylab,xlab = xlab, pch=pch,col=1:15, xlim = xlim, ylim = ylim)
+    legend("topleft",legend=names(Columns[,y]), col =1:15,pch=19)
+    matlines(Number,Columns[,y], col=1:15)
+  }
+  else 
+  {
+    PureSteps <- seq(from=2,to=Length-2,by=3)
+    LowSteps <- seq(from=3,to=Length-1,by=3)
+    HighSteps <- seq(from=4,to=Length,by=3)
+    PureDat <- Data[,c(1,PureSteps)]
+    LowDat <- Data[,c(1,LowSteps)]
+    HighDat <- Data[,c(1,HighSteps)]
+    PureNames <- names(PureDat)
+    substr(names(PureDat), 1, 6) <- ""
+    substr(names(PureDat), 1, 6) <- ""
+    substr(names(PureDat), 1, 6) <- ""
+    substr(names(PureDat), 1, 6) <- ""
+    substr(names(PureDat), 1, 6) <- ""
+    substr(names(PureDat), 1, 6) <- ""
+    names(PureDat)[1] <- "Number"
+    PureColumns <- PureDat[-1]
+    matplot(Number,PureColumns[,y],ylab= ylab,xlab = xlab, pch=pch,col=1:15, xlim = xlim, ylim = ylim)
+    legend("topleft",legend=names(PureColumns[,y]), col =1:15,pch=19)
+    matlines(Number,PureColumns[,y], col=1:15)
+    LowColumns<- LowDat[-1]
+    matlines(Number,LowColumns[,y], col=1:15, lwd=.2)
+    HighColumns<- HighDat[-1]
+    matlines(Number,HighColumns[,y], col=1:15, lwd=.2)
+  }
+}
