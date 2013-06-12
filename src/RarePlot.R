9702fe64bc677dc053cc1a7049351b7586f4ed44
andrew <ach52@pitt.edu>
2013-06-12 15:07:32 -0400

    Updated Stacked Bar Chart and RarePlot w/ Documentation

--- src/RarePlot.R
+++ src/RarePlot.R
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 RarePlot <- function(x,y,ylab= "Number of Different OTUs",xlab= "Number of Tags Sampled",pch= NA, xlim = NULL, ylim = NULL, error=FALSE) #Same Defaults as matplot
 {
   library(reshape2)
@@ -9,10 +10,24 @@ RarePlot <- function(x,y,ylab= "Number of Different OTUs",xlab= "Number of Tags 
     Steps <- seq(from=2,to=Length-2,by=3)
     TrueDat <- Data[,c(1,Steps)]
     Names <- names(TrueDat)
+=======
+rarePlot <- function(file, groups, ylab = "Number of Different OTUs", xlab = "Number of Tags Sampled",
+                     pch = NA, xlim = NULL, ylim = NULL, error = FALSE){ 
+  library(reshape2)
+  require(ggplot2)                  # install.packages("ggplot2")
+  Data                           <- read.table(file, header = T ) # put Data in Dataframe
+  Length                         <- length(Data[1, ]) 
+  if (error == FALSE) {  # If we don't want errorbars  
+    Steps                        <- seq(from = 2, to = Length - 2, by = 3)  # Select only the value not the Lower or Upper Bound
+    TrueDat                      <- Data[, c(1, Steps)] 
+    substr(names(TrueDat), 1, 6) <- "" # This deletes the first character of the groupname
+    substr(names(TrueDat), 1, 6) <- ""  # Repeat 6 times so you only have the name
+>>>>>>> Updated Stacked Bar Chart and RarePlot w/ Documentation
     substr(names(TrueDat), 1, 6) <- ""
     substr(names(TrueDat), 1, 6) <- ""
     substr(names(TrueDat), 1, 6) <- ""
     substr(names(TrueDat), 1, 6) <- ""
+<<<<<<< HEAD
     substr(names(TrueDat), 1, 6) <- ""
     substr(names(TrueDat), 1, 6) <- ""
     names(TrueDat)[1] <- "Number"
@@ -31,12 +46,29 @@ RarePlot <- function(x,y,ylab= "Number of Different OTUs",xlab= "Number of Tags 
     LowDat <- Data[,c(1,LowSteps)]
     HighDat <- Data[,c(1,HighSteps)]
     PureNames <- names(PureDat)
+=======
+    names(TrueDat)[1]            <- "Number"
+    Number                       <- TrueDat[1]
+    Columns                      <- TrueDat[-1]
+    matplot(Number, Columns[, groups], ylab = ylab, xlab = xlab, pch = pch, col = 1:15, xlim = xlim, ylim = ylim) #Plot the groups you want
+    legend("topleft", legend = names(Columns[, groups]), col = 1:15, pch = 19)  # Make a legend
+    matlines(Number, Columns[, groups], col = 1:15)  # Make lines conecting the points
+  }
+  else {  # If we do want errorbars
+    PureSteps                    <- seq(from = 2, to = Length - 2, by = 3)
+    LowSteps                     <- seq(from = 3, to = Length - 1, by = 3)  # Select only the lower bounds
+    HighSteps                    <- seq(from = 4, to = Length, by = 3)  # Select only the upper bounds
+    PureDat                      <- Data[ , c(1, PureSteps)]
+    LowDat                       <- Data[ , c(1, LowSteps)]
+    HighDat                      <- Data[ , c(1, HighSteps)]
+>>>>>>> Updated Stacked Bar Chart and RarePlot w/ Documentation
     substr(names(PureDat), 1, 6) <- ""
     substr(names(PureDat), 1, 6) <- ""
     substr(names(PureDat), 1, 6) <- ""
     substr(names(PureDat), 1, 6) <- ""
     substr(names(PureDat), 1, 6) <- ""
     substr(names(PureDat), 1, 6) <- ""
+<<<<<<< HEAD
     names(PureDat)[1] <- "Number"
     PureNumber <- PureDat[1]
     PureColumns <- PureDat[-1]
@@ -49,10 +81,23 @@ RarePlot <- function(x,y,ylab= "Number of Different OTUs",xlab= "Number of Tags 
     LowNumber <- LowDat[1]
     LowColumns <- LowDat[-1]
     HighNames <- names(LowDat)
+=======
+    names(PureDat)[1]            <- "Number"
+    PureNumber                   <- PureDat[1]
+    PureColumns                  <- PureDat[-1]
+    substr(names(LowDat), 1, 6)  <- ""
+    substr(names(LowDat), 1, 6)  <- ""
+    substr(names(LowDat), 1, 6)  <- ""
+    substr(names(LowDat), 1, 6)  <- "" 
+    names(LowDat)[1]             <- "Number"
+    LowNumber                    <- LowDat[1]
+    LowColumns                   <- LowDat[-1]
+>>>>>>> Updated Stacked Bar Chart and RarePlot w/ Documentation
     substr(names(HighDat), 1, 6) <- ""
     substr(names(HighDat), 1, 6) <- ""
     substr(names(HighDat), 1, 6) <- ""
     substr(names(HighDat), 1, 6) <- ""
+<<<<<<< HEAD
     names(HighDat)[1] <- "Number"
     HighNumber <- HighDat[1]
     HighColumns <- HighDat[-1]
@@ -65,3 +110,18 @@ RarePlot <- function(x,y,ylab= "Number of Different OTUs",xlab= "Number of Tags 
     matlines(HighNumber,HighColumns[,y], col=1:15, lwd=.2)
   }
 }
+=======
+    names(HighDat)[1]            <- "Number"
+    HighNumber                   <- HighDat[1]
+    HighColumns                  <- HighDat[-1]
+    matplot(PureNumber, PureColumns[, y], ylab = ylab, xlab = xlab, pch = pch,
+            col = 1:15, xlim = xlim, ylim = ylim)  # Plot center points
+    legend("topleft", legend = names(PureColumns[ , y]), col = 1:15, pch = 19)  # Plot Legend
+    matlines(PureNumber, PureColumns[, y], col = 1:15)  # Plot center Line
+    LowColumns                   <- LowDat[-1]
+    matlines(LowNumber, LowColumns[, y], col = 1:15, lwd = .2)  # Plot faint Lowbound line
+    HighColumns                  <- HighDat[-1]
+    matlines(HighNumber, HighColumns[, y], col = 1:15, lwd = .2)  # Plot faint Highbound line
+  }
+}     
+>>>>>>> Updated Stacked Bar Chart and RarePlot w/ Documentation
--- src/StackedBarChartsForSharedAndDesign.R
+++ src/StackedBarChartsForSharedAndDesign.R
@@ -1,16 +1,17 @@
-StackBar.SDP <- function(x,y,z){
-SampleData <- read.table(x,header=T)
-Partitions <- read.table(y,header=F)
-StoolData <- cbind(Partitions[,2],SampleData[,-(1:3)])
-OrderData <- StoolData[order(Partitions[,2]),]
+stackBarSDP         <- function(shared, design, cutoff, legend.space = 1.1){
+SampleData          <- read.table(shared, header = T)
+Partitions          <- read.table(design, header = F)
+StoolData           <- cbind(Partitions[, 2], SampleData[, - (1:3)])  #  Combine the Data Frames 
+OrderData           <- StoolData[order(Partitions[, 2]), ]            # While removing extraneous Data
 names(OrderData)[1] <- "Partition"
-PartedData <-rowsum(OrderData[,-1],OrderData$Partition)
-SummedData <-rowSums(PartedData)#Total Bacteria In Each RowPartedD
-PercentData <- PartedData/SummedData
-PercentData[PercentData < z] <- 0
-Other <- 1-rowSums(PercentData)
-SemiFinalData <- cbind(PercentData,Other)
-FinalData <- SemiFinalData[,!(colSums(abs(SemiFinalData)) == 0)]
-par(xpd=F,mar=c(4,4,4,6))
-Plot <- barplot(t(FinalData),legend=T,ylim=c(0,1),col=rainbow(length(FinalData)),space=0.1,args.legend = list(x=length(FinalData[,1])+1.1))
+PartedData          <- rowsum(OrderData[, -1],OrderData$Partition)
+SummedData          <- rowSums(PartedData)  # Total amount In Each RowParted
+PercentData         <- PartedData / SummedData
+PercentData[PercentData < cutoff] <- 0
+Other               <- 1 - rowSums(PercentData)
+SemiFinalData       <- cbind(Other, PercentData)
+FinalData           <- SemiFinalData[, !(colSums(abs(SemiFinalData)) == 0)]
+par(xpd = F, mar = x <- c(4, 4, 4, 6))
+barplot(t(FinalData), legend = T, ylim = c(0, 1), col = rainbow(length(FinalData)),
+        space = 0.1, args.legend = list(x = length(FinalData[, 1]) + legend.space))
 }
