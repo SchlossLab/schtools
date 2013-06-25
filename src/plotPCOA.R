35d91fac02aee5e3f9de5b736d98df037c5786c2
andrew <ach52@pitt.edu>
2013-06-25 14:26:42 -0400

    Uploading 2 pcoa functions
    Trying this again

---
+++ src/plotPCOA.R
@@ -0,0 +1,18 @@
+plotPCOA<- function(axes,design=FALSE, ax1=1, ax2=2, Lab1="axis1", Lab2= "axis2",...,  psize=I(4.5)){
+  require(ggplot2) 
+  axi             <- read.table(axes,header = T)
+  if (design==FALSE){
+    qplot(data= axi,x= axis1, y=axis2, xlab=Lab1, ylab=Lab2, size = I(psize))
+  }
+  else {
+    design         <- suppressWarnings(read.table(design, header=F))
+    tDesign        <- t(design)
+    taxi           <- cbind(tDesign[,2],axi)
+    names(taxi)[1] <- "partition" 
+    groups         <- taxi[1]
+    theme_set(theme_gray(base_size = 16))
+    
+      qplot(data=taxi, x=axis1, y=axis2, color=partition,..., size = I(psize))
+   
+  }
+}
