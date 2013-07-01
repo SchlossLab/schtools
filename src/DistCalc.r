824f74e61720fac11cc1b14829041a55bcb8d59e
andrew <ach52@pitt.edu>
2013-07-01 15:53:14 -0400

    updating dist.calc
    adding comments

--- src/DistCalc.r
+++ src/DistCalc.r
@@ -82,20 +82,20 @@ between.dist<-function(dist,design,groups){
         dists<-get.dists(dist,design,group1,group2)   # use get.dists using the dist,design and the 2 groups
         cat(sprintf("Stats for distances between %s and %s", group1, group2), "\n", # Stats for distances between grp1 and grp 2
             sprintf('Minimum: %f', min(dists)), "\n", sprintf('Maximum: %f', max(dists)),  # minimum: (mindists)
-            "\n", sprintf('Median: %f', median(dists)), "\n",sprintf('Mean: %f', mean(dists)),  #maximum: (maxdists) ; Mean: (mean(dists))
-            "\n", sprintf('Std. Dev.: %f', sd(dists)), "\n", "\n", sep='') 
-        return(dists) return this
+            "\n", sprintf('Median: %f', median(dists)), "\n",sprintf('Mean: %f', mean(dists)),  # maximum: (maxdists) ; Mean: (mean(dists))
+            "\n", sprintf('Std. Dev.: %f', sd(dists)), "\n", "\n", sep='') # Std.Dev.: (sd(dists))
+        return(dists) # return this
     }  
      
-    dist.vect<-c()
+    dist.vect<-c() # 
     c<-1 # start c for the loop
-    dist.list<-vector(mode='list',length=((length(groups)^2-length(groups))/2)) #  
-    list.names<-c()
-    for(i in 1:(length(groups)-1)){
-        for(j in (i+1):length(groups)){
-            list.names[c]<-paste(groups[i], 'vs', groups[j])
-            dist.list[[c]]<-one.between(dist,design,groups[i],groups[j])
-            c<-c+1
+    dist.list<-vector(mode='list',length=((length(groups)^2-length(groups))/2)) #  length(groups=lg), length= lg^2-lg/2  
+    list.names<-c() # prep list.names for the loop
+    for(i in 1:(length(groups)-1)){ # 
+        for(j in (i+1):length(groups)){ #for every j in 2 through end of groups 
+            list.names[c]<-paste(groups[i], 'vs', groups[j]) # list of names i vs j
+            dist.list[[c]]<-one.between(dist,design,groups[i],groups[j]) # get the in between data of i and j
+            c<-c+1 #loop through it
         }
     }
    
@@ -103,14 +103,14 @@ between.dist<-function(dist,design,groups){
     invisible(dist.list)
 }
 #Returns min, max, mean, median, and sd of distance within a group
-within.dist<-function(dist,design,groups){
+within.dist<-function(dist,design,groups){ # 
     
-    if(length(groups)==1){
-        options(warn=(-1))    
-        if(groups=='all'){
-            if(length(design)==1){dsn<-read.table(design)}
-            if(length(design)>1){dsn<-design}
-            groups<-as.vector(unique(dsn[,2]))
+    if(length(groups)==1){ # if only one group
+        options(warn=(-1))   #  wait till end to show warnings
+        if(groups=='all'){ # if groups are all
+            if(length(design)==1){dsn<-read.table(design)} # if the design file is just 
+            if(length(design)>1){dsn<-design} # if length is greater thn one then dsn isthe design file.
+            groups<-as.vector(unique(dsn[,2])) # set the diffrent group names into a file.
             options(warn=(0))    
         }
     }
@@ -119,96 +119,96 @@ within.dist<-function(dist,design,groups){
         if(length(design)==1) { design<-read.design(design) }
         
         get.dists<-function(dist,design,group1,group2){
-            get.grp1<-function(d , g){ #given a design and a group for every row with group=g  
-                c1<-1
-                grp1<-c()
-                for(i in 1:nrow(d)){
-                    if(d[i,2]==g){
-                        grp1[c1]<-d[i,1]
-                        c1<-c1+1
+            get.grp1<-function(d , g){   # given a design and a group for every row with group=g  get all in one group toghether
+                c1<-1                    # prep the loop
+                grp1<-c()                # prep grp1 for the loop
+                for(i in 1:nrow(d)){     # for every row in d
+                    if(d[i,2]==g){       # if the 2nd column value is g
+                        grp1[c1]<-d[i,1] # add it to the group 1 list
+                        c1<-c1+1         # move the next slot in the group 1 list down 1
                     }
                 }
                 invisible(grp1)
             }
-            get.grp2<-function(d, g){ 
-                c2<-1
-                grp2<-c()
-                for(i in 1:nrow(d)){
-                    if(d[i,2]==g){
-                        grp2[c2]<-d[i,1]
-                        c2<-c2+1
+            get.grp2<-function(d, g){    # similar to above but a 2nd group
+                c2<-1                    # Prep c=2
+                grp2<-c()                #  
+                for(i in 1:nrow(d)){     # 
+                    if(d[i,2]==g){       # 
+                        grp2[c2]<-d[i,1] # 
+                        c2<-c2+1         # 
                     }
                 }
-                invisible(grp2)
+                invisible(grp2)#
             }
             
-            grp1<-get.grp1(design,group1)
-            grp2<-get.grp2(design,group2)
+            grp1<-get.grp1(design,group1) # get group 1#
+            grp2<-get.grp2(design,group2) # get group 2
             
-            get.rows<-function(dist, grp){
-                rows<-dist[grp[1],]
-                for(i in 2:length(grp)){
-                    rows<-rbind(rows,dist[grp[i],])      
+            get.rows<-function(dist, grp){ # Function
+                rows<-dist[grp[1],]        # every row with a name in group1
+                for(i in 2:length(grp)){   # for every row with a name in group 1 
+                    rows<-rbind(rows,dist[grp[i],]) #   
                 }
                 invisible(rows)
             }
             rows<-get.rows(dist,grp1)
             
-            get.rows<-function(dist, grp){
-                matrix<-dist[,grp[1]]
-                for(i in 2:length(grp)){
-                    matrix<-cbind(matrix,dist[,grp[i]])      
+            get.rows<-function(dist, grp){ # 
+                matrix<-dist[,grp[1]] # all of the names of grp 1
+                for(i in 2:length(grp)){# for every i in the length of grp
+                    matrix<-cbind(matrix,dist[,grp[i]])      # columnbind (names of grp1 with every column of grp)
                 }
-                invisible(matrix)
+                invisible(matrix) #
             }
-            matrix<-get.rows(rows,grp2)
-            vect<-as.vector(matrix)
-            invisible(vect)
+            matrix<-get.rows(rows,grp2) # the 2 grps compared to each ther
+            vect<-as.vector(matrix) #
+            invisible(vect) #
         }
-        dists<-get.dists(dist,design,group,group)  
-        rm.duplicates<-function(dists){
-            square<-matrix(dists,nrow=sqrt(length(dists)))
-            half.dists<-c()
-            half.dists[1]<-square[2,1]
-            for(i in 3:ncol(square)){
-                keepers<-square[i,1:(i-1)] 
-                half.dists[(length(half.dists)+1):(length(half.dists)+length(keepers))] <- keepers
-            }
-            invisible(half.dists)
+        dists<-get.dists(dist,design,group,group)  #
+        rm.duplicates<-function(dists){#
+            square<-matrix(dists,nrow=sqrt(length(dists)))#
+            half.dists<-c()#
+            half.dists[1]<-square[2,1]#
+            for(i in 3:ncol(square)){#
+                keepers<-square[i,1:(i-1)] #
+                half.dists[(length(half.dists)+1):(length(half.dists)+length(keepers))] <- keepers#
+            }#
+            invisible(half.dists)#
         }
-        lt.dist<-rm.duplicates(dists)
-        cat(sprintf("Stats for distances within %s", group), "\n", sprintf('Minimum: %f', min(lt.dist)),
-            "\n", sprintf('Maximum: %f', max(lt.dist)), "\n", sprintf('Median: %f', median(lt.dist)),
-            "\n",sprintf('Mean: %f', mean(lt.dist)), "\n", sprintf('Std. Dev.: %f', sd(lt.dist)),
-            "\n", sprintf('Std. Error.:%f', sd(lt.dist)/(sqrt(nrow(design)-1))), "\n", "\n", sep='')
-        invisible(lt.dist)
+        lt.dist<-rm.duplicates(dists)#
+        cat(sprintf("Stats for distances within %s", group), "\n", sprintf('Minimum: %f', min(lt.dist)),#  add all of these together
+            "\n", sprintf('Maximum: %f', max(lt.dist)), "\n", sprintf('Median: %f', median(lt.dist)),#
+            "\n",sprintf('Mean: %f', mean(lt.dist)), "\n", sprintf('Std. Dev.: %f', sd(lt.dist)),#
+            "\n", sprintf('Std. Error.:%f', sd(lt.dist)/(sqrt(nrow(design)-1))), "\n", "\n", sep='')#
+        invisible(lt.dist)#
         print(lt.dist)
     }
-    dist.vect<-c()
-    c<-1
-    dist.list<-vector(mode='list',length=length(groups))
-    for(i in groups){
-        dist.list[[c]]<-one.within(dist,design,i)
-        c<-c+1
+    dist.vect<-c()# 
+    c<-1# start the loop
+    dist.list<-vector(mode='list',length=length(groups)) # 
+    for(i in groups){ #
+        dist.list[[c]]<-one.within(dist,design,i) #
+        c<-c+1 #
     }
     
-    names(dist.list)<-groups
-    invisible(dist.list)
+    names(dist.list)<-groups#
+    invisible(dist.list)#
 }
 
 
 #Makes a barplot using the output from either between.dist or within.dist
-plot.dists<-function(list, space=NULL, col=c(), ylim=c(0,1), ylab='Average Distance', xlab='', main=c()){
-    avgs<-c()
-    for(i in 1:length(list)){
-        avgs[i]<-mean(list[[i]])
+plot.dists<-function(list, space=NULL, col=c(), ylim=c(0,1), ylab='Average Distance', xlab='', main=c()){#
+    avgs<-c()#
+    for(i in 1:length(list)){#
+        avgs[i]<-mean(list[[i]])#
     }
     
-    if(length(col)==0){col<-rgb(runif(length(list)),runif(length(list)),runif(length(list)))}
-    xcor<-barplot(avgs,names.arg=names(list), space=space, ylim=ylim, col=col, xlab=xlab, ylab=ylab, main=main)
+    if(length(col)==0){col<-rgb(runif(length(list)),runif(length(list)),runif(length(list)))}#
+    xcor<-barplot(avgs,names.arg=names(list), space=space, ylim=ylim, col=col, xlab=xlab, ylab=ylab, main=main)#
     
-    for(i in 1:length(list)){
-        arrows(x0=xcor[i], x1=xcor[i], y0=mean(list[[i]]), y1=mean(list[[i]])+sd(list[[i]]), angle=90)
+    for(i in 1:length(list)){#
+        arrows(x0=xcor[i], x1=xcor[i], y0=mean(list[[i]]), y1=mean(list[[i]])+sd(list[[i]]), angle=90)#
     }
 }
 
