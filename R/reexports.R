c40065f60e3c35c2550ccaf7db1b59200d76c650
Kelly Sovacool <kellysovacool@gmail.com>
2020-10-13 13:51:48 -0400

    Re-exports are already in reexports.R

--- R/reexports.R
+++ R/reexports.R
@@ -7,3 +7,7 @@ dplyr::`%>%`
 #' @importFrom rlang .data
 #' @export
 rlang::.data
+
+## make R CMD CHECK shut up about the dot `.``
+## See: \url{https://github.com/tidyverse/magrittr/issues/29}
+utils::globalVariables(c("."))
--- R/utils.R
+++
@@ -1,8 +0,0 @@
-#' dplyr pipe
-#' @importFrom dplyr %>%
-#' @export
-dplyr::`%>%`
-
-## make R CMD CHECK shut up about the dot `.``
-## See: \url{https://github.com/tidyverse/magrittr/issues/29}
-utils::globalVariables(c("."))
