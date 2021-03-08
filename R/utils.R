6f7acc8c6eaeb64c65c651338c92c3d14a740346
Kelly Sovacool <kelly-sovacool@users.noreply.github.com>
2021-03-08 13:52:17 -0500

    Fix typo

--- R/utils.R
+++ R/utils.R
@@ -3,7 +3,7 @@
 #' @export
 dplyr::`%>%`
 
-## make R CMD CHECK shut up about the dot `.``
+## make R CMD CHECK shut up about the dot `.`
 ## See: \url{https://github.com/tidyverse/magrittr/issues/29}
 utils::globalVariables(c("."))
 
