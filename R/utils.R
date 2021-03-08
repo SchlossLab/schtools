828b97956da9df6b8bb07f830320fd32f3594969
Kelly Sovacool <kelly-sovacool@users.noreply.github.com>
2021-03-08 13:51:49 -0500

    Add function to install & load packages

--- R/utils.R
+++ R/utils.R
@@ -6,3 +6,12 @@ dplyr::`%>%`
 ## make R CMD CHECK shut up about the dot `.``
 ## See: \url{https://github.com/tidyverse/magrittr/issues/29}
 utils::globalVariables(c("."))
+
+#' Install & load packages
+load_deps <- function(deps) {
+  not_installed <- names(Filter(isFALSE, sapply(deps, requireNamespace, 
+                                                quietly = TRUE)))
+  install.packages(not_installed, quiet = TRUE, dependencies = TRUE,
+                   repos = "http://cran.us.r-project.org")
+  invisible(sapply(deps, library, verbose = FALSE, character.only = TRUE))
+}
