13290c93837025ab536ebec3ed0471cfe48a250d
Kelly Sovacool <kellysovacool@gmail.com>
2021-05-19 13:40:33 -0400

    Rename the package to 'schtools'

--- R/utils.R
+++ R/utils.R
@@ -8,11 +8,14 @@ dplyr::`%>%`
 utils::globalVariables(c("."))
 
 #' Install & load packages
+#'
+#' @param ... names of packages to install
+#' @export
 load_deps <- function(...) {
   deps <- c(...)
-  not_installed <- names(Filter(isFALSE, sapply(deps, requireNamespace, 
+  not_installed <- names(Filter(isFALSE, sapply(deps, requireNamespace,
                                                 quietly = TRUE)))
-  install.packages(not_installed, quiet = TRUE, dependencies = TRUE,
+  utils::install.packages(not_installed, quiet = TRUE, dependencies = TRUE,
                    repos = "http://cran.us.r-project.org")
   invisible(sapply(deps, library, verbose = FALSE, character.only = TRUE))
 }
--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,4 @@
 library(testthat)
-library(mothuR)
+library(schtools)
 
-test_check("mothuR")
+test_check("schtools")
