176297fd8c06e6f4f35e2f80420021d86d793fb7
Kelly Sovacool <kelly-sovacool@users.noreply.github.com>
2021-09-24 23:01:38 -0400

    Remove duplicate re-exports

--- R/utils.R
+++ R/utils.R
@@ -1,12 +1,3 @@
-#' dplyr pipe
-#' @importFrom dplyr %>%
-#' @export
-dplyr::`%>%`
-
-## make R CMD CHECK shut up about the dot `.`
-## See: \url{https://github.com/tidyverse/magrittr/issues/29}
-utils::globalVariables(c("."))
-
 #' Install & load packages
 #' @param ... package names to install & load
 #' @export
