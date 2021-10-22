8adef995dd37564bbf5a3338267f9068ceaf5e5c
Kelly Sovacool <kellysovacool@gmail.com>
2021-10-22 18:13:35 -0400

    Check whether 2 numeric vectors are close_enough()
    Really just a shortcut to all(dplyr::near(x, y, tol=10^-3) for less
    precision.

--- R/utils.R
+++ R/utils.R
@@ -7,6 +7,41 @@ dplyr::`%>%`
 ## See: \url{https://github.com/tidyverse/magrittr/issues/29}
 utils::globalVariables(c("."))
 
+#' Checks whether a number is near to a whole number
+#'
+#' @param x a numeric
+#'
+#' @return `TRUE` or `FALSE`
+#' @export
+#'
+#' @examples
+#' is_nearly_whole(.Machine$double.eps^0.5)
+#' is_nearly_whole(.Machine$double.eps^0.6)
+#' is_nearly_whole(1)
+is_nearly_whole <- function(x) {
+  abs(x - round(x)) < .Machine$double.eps^0.5
+}
+
+#' Check whether two numeric vectors are close enough for gov't work.
+#'
+#' This like `dplyr::near()` except with much less precision.
+#'
+#' @param x a numeric vector
+#' @param y another numeric vector
+#' @param tol tolerance (default: `10^-3`.)
+#'
+#' @return `TRUE` if all numbers are near enough within the tolerance, otherwise `FALSE`
+#' @export
+#'
+#' @examples
+#'
+#' close_enough(0.0004, 0)
+#' close_enough(0.8887, 0.8884)
+#' close_enough(1, 2)
+close_enough <- function(x, y, tol = 10^-3) {
+  all(dplyr::near(x, y, tol = tol))
+}
+
 #' Install & load packages
 #' @param ... package names to install & load
 #' @export
