91fbdb85a6c4ea6b473b231f011da3b4108c8eaa
Kelly Sovacool <kellysovacool@gmail.com>
2021-10-25 14:06:17 -0400

    Add author

--- R/utils.R
+++ R/utils.R
@@ -4,6 +4,7 @@
 #'
 #' @return `TRUE` or `FALSE`
 #' @export
+#' @author Kelly Sovacool \email{sovacool@@umich.edu}
 #'
 #' @examples
 #' is_nearly_whole(.Machine$double.eps^0.5)
@@ -23,6 +24,7 @@ is_nearly_whole <- function(x) {
 #'
 #' @return `TRUE` if all numbers are near enough within the tolerance, otherwise `FALSE`
 #' @export
+#' @author Kelly Sovacool \email{sovacool@@umich.edu}
 #'
 #' @examples
 #'
