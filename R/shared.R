e215c3aa4a369168b642ce909bea40a1cb89768e
Kelly Sovacool <kelly-sovacool@users.noreply.github.com>
2022-10-05 16:30:39 -0400

    Refer to doi link with \doi{}

--- R/shared.R
+++ R/shared.R
@@ -2,7 +2,7 @@
 #' Pool OTU counts at a particular taxonomic level
 #'
 #' Enables comparing analyses at different taxonomic resolutions,
-#' as seen in <https://doi.org/10.1128/mbio.03161-21>.
+#' as seen in \doi{10.1128/mbio.03161-21}.
 #' Implementation adapted from [here](https://github.com/SchlossLab/Armour_Resolution_mBio_2021/blob/master/code/get_phylotype_shared.R).
 #'
 #' @param otu_shared_dat data frame created from a [shared file](https://mothur.org/wiki/shared_file/)
